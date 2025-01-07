5628a3750a5480b36799d29ed752f681fed5507e
rebeccapeltz <rebeccapeltz@gmail.com>
2025-01-06 17:45:01 -0800

    add link to header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
 function Header() {
   return (
     <div className="flex justify-between my-8 items-center">
-      <h1 className="font-medium text-xl">Ski Antler</h1>
+      <a
+        href="/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h1 className="font-medium text-xl">Ski Antler</h1>
+      </a>
       <div>
         <a
           href="https://github.com/rebeccapeltz/ski-antler"
